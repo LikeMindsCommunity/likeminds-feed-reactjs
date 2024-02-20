639a300daf5c2f5b9c4a5fb6ae85681d9f30bb01
sanjaylikeminds <sanjay.kumar@likeminds.community>
2024-02-21 00:53:18 +0530

    parser added.

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,43 @@
-const Feed = () => {
+import React from "react";
+import { Post } from "../types/models/post";
+import { User } from "../types/models/member";
+import { formatTimeAgo, getAvatar } from "../shared/utils";
+import { parseAndReplaceTags } from "../shared/taggingParser";
+
+interface FeedProps {
+  post: Post;
+  user: User | undefined;
+}
+
+const Feed: React.FC<FeedProps> = (props) => {
+  console.info(props.post);
+  const { text, createdAt } = props.post; // Assuming Post has these properties
+  const { name, imageUrl, customTitle } = props.user || {};
+
+  // Determine the avatar content based on imageUrl and name
+  const avatarContent = getAvatar({ imageUrl, name });
+
   return (
-    <div className="lmFeedWrapper">
-      <h1>Feed Route Flat</h1>
+    <div className="lm-feed-wrapper__card lm-mb-2">
+      <div className="lm-feed-wrapper__card__header">
+        <div className="lm-flex-container">
+          <div className="lm-avatar lm-mr-5">{avatarContent}</div>
+          <div>
+            <div className="lm-feed-wrapper__card__header--title">
+              {name}
+              <span>{customTitle}</span>
+            </div>
+            <div>Post {formatTimeAgo(createdAt)}</div>
+          </div>
+        </div>
+        <div>right</div>
+      </div>
+      <div className="lm-feed-wrapper__card__body">
+        {parseAndReplaceTags(text)}
+      </div>
+      <div className="lm-feed-wrapper__card__footer">
+        {/* <h1>{footer}</h1> */}
+      </div>
     </div>
   );
 };
--- src/components/LMFlatFeed.tsx
+++ src/components/LMFlatFeed.tsx
@@ -33,21 +33,27 @@ const LMFlatFeed = (props: LMFlatFeedProps) => {
   } = useFetchFeeds();
   const renderFeeds = useCallback(() => {
     return feedList.map((post: Post) => {
+      const postUuid = post.uuid;
+      const usersArray = Object.values(feedUsersList);
+      const filteredUser = usersArray.find((user) => user.uuid === postUuid);
+
       return (
         <FeedPostContext.Provider
+          key={post._id}
           value={{
             post: post,
             users: feedUsersList,
             topics: topics,
           }}
         >
-          <>{post.text}</>
+          <Feed post={post} user={filteredUser} />
         </FeedPostContext.Provider>
       );
     });
   }, [feedList, feedUsersList, topics]);
+
   return (
-    <div>
+    <div className="lm-feed-wrapper">
       <InfiniteScroll
         dataLength={feedList.length}
         hasMore={loadMoreFeeds}
