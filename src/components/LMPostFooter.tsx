4af401cada00712bf2cf549208bb3a8c95ad7459
sanjaylikeminds <sanjay.kumar@likeminds.community>
2024-03-09 00:36:41 +0530

    share and bookmark icon hide.

--- src/components/LMPostFooter.tsx
+++ src/components/LMPostFooter.tsx
@@ -3,8 +3,8 @@ import { CustomAgentProviderContext } from "../contexts/LMCustomAgentProviderCon
 import { LMLikeAction } from "../shared/actions";
 import like from "../assets/images/like.svg";
 import commnent from "../assets/images/comment.svg";
-import bookmark from "../assets/images/bookmark.svg";
-import share from "../assets/images/share.svg";
+// import bookmark from "../assets/images/bookmark.svg";
+// import share from "../assets/images/share.svg";
 import { FeedPostContext } from "../contexts/LMFeedPostContext";
 import { ROUTES } from "../shared/constants/lmRoutesConstant";
 import { Link } from "react-router-dom";
@@ -58,12 +58,12 @@ const LMPostFooter = () => {
             </div>
           </div>
           <div className="lm-social-action-bar__actions">
-            <div className="lm-d-flex lm-align-items-center lm-cursor-pointer">
+            {/* <div className="lm-d-flex lm-align-items-center lm-cursor-pointer">
               <img src={bookmark} alt="bookmark" />
             </div>
             <div className="lm-d-flex lm-align-items-center lm-cursor-pointer">
               <img src={share} alt="share" />
-            </div>
+            </div> */}
           </div>
         </div>
         <LMCommentsScroller />
