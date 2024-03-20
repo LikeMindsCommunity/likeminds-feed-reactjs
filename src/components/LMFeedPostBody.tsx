22622026ff637599941c163c4d9739afb8f75945
omenUchiha <gaurav.rajput@likeminds.community>
2024-03-20 14:37:13 +0530

    added read more

--- src/components/LMFeedPostBody.tsx
+++ src/components/LMFeedPostBody.tsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { parseAndReplaceTags } from "../shared/taggingParser";
+import { useContext, useState } from "react";
+import { parseAndReplaceTags, textPreprocessor } from "../shared/taggingParser";
 import { FeedPostContext } from "../contexts/LMFeedPostContext";
 import LMFeedAttachments from "../shared/components/LMFeedAttachments";
 import { CustomAgentProviderContext } from "../contexts/LMFeedCustomAgentProviderContext";
@@ -8,6 +8,8 @@ const LMFeedPostBody = () => {
   const { post } = useContext(FeedPostContext);
   const { text, attachments, heading } = post!;
   const { LMPostBodyStyles } = useContext(CustomAgentProviderContext);
+  const [hasReadMoreTapped, setHasReadMoreTapped] = useState<boolean>(false);
+
   // Render attachments
   const renderAttachments = () => {
     if (!attachments || attachments.length === 0) return null;
@@ -33,7 +35,31 @@ const LMFeedPostBody = () => {
         className="lm-feed-wrapper__card__body__content"
         style={LMPostBodyStyles?.content}
       >
-        {parseAndReplaceTags(text)}
+        {/* {parseAndReplaceTags(text)} */}
+        {(() => {
+          const processedText = textPreprocessor(text);
+          if (processedText.showReadMore && !hasReadMoreTapped) {
+            return (
+              <>
+                {parseAndReplaceTags(processedText.text)}
+                <span
+                  className="lm-feed-wrapper__card__body__content__read-more-tap-icon"
+                  style={{
+                    color: "gray",
+                    fontWeight: "400",
+                    cursor: "pointer",
+                    fontSize: "14px",
+                  }}
+                  onClick={() => setHasReadMoreTapped(true)}
+                >
+                  ...ReadMore
+                </span>
+              </>
+            );
+          } else {
+            return parseAndReplaceTags(text);
+          }
+        })()}
       </div>
       <div className="lm-feed-wrapper__card__body__attachment">
         {renderAttachments()}
