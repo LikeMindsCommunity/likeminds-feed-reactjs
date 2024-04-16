fb422ada0ec60d998b3cfe41342c6e1569c79567
sanjaylikeminds <sanjay.kumar@likeminds.community>
2024-04-17 02:01:22 +0530

    all members ui bug fixed

--- src/components/LMFeedAllMembers.tsx
+++ src/components/LMFeedAllMembers.tsx
@@ -1,95 +1,75 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useContext, useEffect, useState } from "react";
 import LMFeedGlobalClientProviderContext from "../contexts/LMFeedGlobalClientProviderContext";
 import { GetAllMembersRequest } from "@likeminds.community/feed-js-beta";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { getAvatar } from "../shared/components/LMUserMedia";
-import { User } from "../shared/types/models/member";
+import { Member } from "../shared/types/models/member";
+import { GetAllMembersResponse } from "../shared/types/api-responses/getAllMembersResponse";
 
 const LMFeedAllMembers = () => {
   const { lmFeedclient, customEventClient } = useContext(
     LMFeedGlobalClientProviderContext,
   );
-  const [members, setMembers] = useState<User[]>();
-  const [loadMoreFeeds, setLoadMoreFeeds] = useState(true);
+  const [members, setMembers] = useState<Member[]>([]);
+  const [loadMoreFeeds, setLoadMoreFeeds] = useState<boolean>(true);
   const [pageCount, setPageCount] = useState<number>(1);
   const [totalMemberCounts, setTotalMemberCounts] = useState<number>(0);
 
-  useEffect(() => {
-    async function getAllMembers() {
-      try {
-        const resData = [];
-
-        // Fetch members for page 1
-        const resPage1 = await lmFeedclient?.getAllMembers(
-          GetAllMembersRequest.builder().setpage(pageCount).build(),
-        );
-
-        // Push members from page 1 into resData array
-        if (resPage1 && resPage1.data && resPage1.data.members) {
-          resData.push(...resPage1.data.members);
-        }
-
-        // Fetch members for page 2
-        const resPage2 = await lmFeedclient?.getAllMembers(
-          GetAllMembersRequest.builder()
-            .setpage(pageCount + 1)
-            .build(),
-        );
-
-        // Push members from page 2 into resData array
-        if (resPage2 && resPage2.data && resPage2.data.members) {
-          resData.push(...resPage2.data.members);
-        }
-
-        // Set members state to the combined array
-        setMembers(resData);
-
-        // Increment pageCount by 1
-        setPageCount((prevPageCount: number) => prevPageCount + 1);
-        console.log("pagecount = ", pageCount);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getAllMembers();
-  }, [lmFeedclient]);
-
-  //   function to load the next page for the current selected topics
-  const getNextPage = async () => {
+  // Function to fetch members, placed outside to be accessible by getNextPage
+  const fetchMembers = async (page: number) => {
     try {
-      const response: any = await lmFeedclient?.getAllMembers(
-        GetAllMembersRequest.builder()
-          .setpage(pageCount + 1)
-          .build(),
-      );
-      setMembers(response?.data?.members);
-      setPageCount((pageCount: number) => pageCount++);
+      const response: GetAllMembersResponse =
+        (await lmFeedclient?.getAllMembers(
+          GetAllMembersRequest.builder().setpage(page).build(),
+        )) as never;
 
-      if (!response?.data?.members.length) {
-        setLoadMoreFeeds(false);
+      if (response && response.data && response.data.members) {
+        if (response.data.members.length > 0) {
+          setMembers((prevMembers) => [
+            ...prevMembers,
+            ...response.data.members,
+          ]);
+          setPageCount(page); // set current page
+          setTotalMemberCounts(response.data.totalMembers);
+        }
+
+        if (
+          response.data.members.length === 0 ||
+          members.length >= response.data.totalMembers
+        ) {
+          setLoadMoreFeeds(false);
+        }
       }
     } catch (error) {
       console.log(error);
+      setLoadMoreFeeds(false);
     }
+  };
+
+  useEffect(() => {
+    fetchMembers(1);
+  }, [lmFeedclient]);
+
+  const getNextPage = () => {
+    fetchMembers(pageCount + 1);
   };
 
   return (
     <div className="lm-member-wrapper">
       <div className="lm-member-wrapper__header">
-        Member List({totalMemberCounts})
+        Member List ({totalMemberCounts})
       </div>
-      <div className="lm-member-wrapper__body">
+      <div className="lm-member-wrapper__body lm-hide-scrollbar">
         <InfiniteScroll
           dataLength={members ? members.length : 0}
           hasMore={loadMoreFeeds}
           next={getNextPage}
           loader={null}
-          useWindow={false}
         >
           {members ? (
-            members.map((member: User) => (
+            members.map((member: Member) => (
               <div key={member.id} className="lm-member-wrapper__body__media">
                 <div className="lm-member-wrapper__body__media__imgBox lm-avatar">
                   {getAvatar({
