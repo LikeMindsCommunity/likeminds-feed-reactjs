dd846bf2175c0f488f340e4fd8b9f56243cabe1a
Gaurav <gaurav.rajput@likeminds.community>
2024-09-13 21:42:53 +0530

    Release v1.3.1 (#86)
    * Updated Package
    
    * updated SDK version

--- core/src/shared/getClient.ts
+++ core/src/shared/getClient.ts
@@ -3,7 +3,7 @@ import { LMFeedClient } from "@likeminds.community/feed-js";
 export function initiateFeedClient() {
   const lmFeedClient = LMFeedClient.Builder()
     .setPlatformCode("rt")
-    .setVersionCode(10)
+    .setVersionCode(11)
     .build();
   return lmFeedClient;
 }
