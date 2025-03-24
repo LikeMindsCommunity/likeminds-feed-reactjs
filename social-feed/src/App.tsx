91da8bdb83265924c0fb8c4817c27d2f3e5e646d
omenUchiha <gaurav.rajput@likeminds.community>
2025-03-24 17:03:33 +0530

    removed non used component

--- social-feed/src/App.tsx
+++ social-feed/src/App.tsx
@@ -9,9 +9,39 @@ import {
 } from "@likeminds.community/likeminds-feed-reactjs";
 
 import LoginScreen from "./LoginScreen";
-import CustomLMFeedCreatePostDialog from "./CustomCreatePostComponent";
 
 function App() {
+  const [accessToken, setAccessToken] = useState<string>("");
+  const [refreshToken, setRefreshToken] = useState<string>("");
+  const [apiKey, setApiKey] = useState<string>("");
+  const [uuid, setUUID] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [noShowScreen, setNoShowScreen] = useState<boolean>(true);
+  const [showLoginScreen, setShowLoginScreen] = useState<boolean>(false);
+  function login() {
+    if (accessToken.length && refreshToken.length) {
+      setUserDetails((userDetails) => {
+        userDetails.accessToken = accessToken;
+        userDetails.refreshToken = refreshToken;
+
+        return userDetails;
+      });
+      setShowLoginScreen(false);
+    } else if (uuid.length && apiKey.length) {
+      setUserDetails((userDetails) => {
+        userDetails.apiKey = apiKey;
+        userDetails.username = username;
+        userDetails.uuid = uuid;
+
+        return userDetails;
+      });
+      setShowLoginScreen(false);
+    } else {
+      alert(
+        "Please provide either API key and UUID OR Access and Refresh Token"
+      );
+    }
+  }
   const customEventClient = new LMFeedCustomEvents();
   const lmCoreCallbacks = new LMCoreCallbacks(
     (a, b) => {},
@@ -22,36 +52,65 @@ function App() {
       };
     }
   );
-
+  const [userDetails, setUserDetails] = useState<{
+    accessToken?: string;
+    refreshToken?: string;
+    uuid?: string;
+    username?: string;
+    isGuest?: boolean;
+    apiKey?: string;
+  }>({});
   const lmFeedClient = initiateFeedClient();
 
+  useEffect(() => {
+    const getLocalUser = localStorage.getItem("LOCAL_USER");
+    if (getLocalUser) {
+      const user = JSON.parse(getLocalUser);
+      const { uuid } = user.sdkClientInfo;
+      const { name } = user;
+      const apiKey = localStorage.getItem("LOCAL_API_KEY");
+      const details = {
+        uuid,
+        username: name,
+        apiKey: apiKey || "",
+        isGuest: false,
+      };
+      setUserDetails(details);
+      setNoShowScreen(false);
+    } else {
+      setNoShowScreen(false);
+      setShowLoginScreen(true);
+    }
+  }, []);
+
+  if (noShowScreen) {
+    return null;
+  }
+
+  if (showLoginScreen) {
+    return (
+      <LoginScreen
+        accessToken={accessToken}
+        refreshToken={refreshToken}
+        setAccessToken={setAccessToken}
+        setRefreshToken={setRefreshToken}
+        apiKey={apiKey}
+        setApiKey={setApiKey}
+        username={username}
+        uuid={uuid}
+        setUUID={setUUID}
+        setUsername={setUsername}
+        login={login}
+      />
+    );
+  }
   return (
     <>
       <LMFeedNotificationHeader customEventClient={customEventClient} />
       <LMSocialFeed
         client={lmFeedClient}
         customEventClient={customEventClient}
-        userDetails={{
-          apiKey: "e7608d57-dcf1-42cd-b320-3d98b1a40cf3",
-          username: "testuser",
-          uuid: "testuser",
-        }}
-        CustomComponents={{
-          CustomCreatePostDialog: <CustomLMFeedCreatePostDialog />,
-        }}
-        PostCreationCustomCallbacks={{
-          postFeedCustomAction: async (store) => {
-            store.defaultActions.postFeed([
-              {
-                content: "Hello, World!",
-                topicIds: ["1"],
-                attachments: [],
-                isAnonymous: false,
-              },
-            ]);
-          },
-        }}
-        // userDetails={userDetails}
+        userDetails={userDetails}
         LMFeedCoreCallbacks={lmCoreCallbacks}
       ></LMSocialFeed>
     </>
