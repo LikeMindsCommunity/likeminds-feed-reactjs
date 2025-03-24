0d6740baf4c8e91654d08e4b1fac38bf141b728c
omenUchiha <gaurav.rajput@likeminds.community>
2025-03-24 17:07:52 +0530

    updated package versions

--- core/src/shared/getClient.ts
+++ core/src/shared/getClient.ts
@@ -3,7 +3,7 @@ import { LMFeedClient } from "@likeminds.community/feed-js";
 export function initiateFeedClient() {
   const lmFeedClient = LMFeedClient.Builder()
     .setPlatformCode("rt")
-    .setVersionCode(17)
+    .setVersionCode(18)
     .build();
   return lmFeedClient;
 }
