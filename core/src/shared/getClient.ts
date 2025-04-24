9d1dc61002f4b9668fe46729a1f3498f96e5b125
Ishaan Jain <ishaan.jain@likeminds.community>
2025-04-24 16:29:08 +0530

    Release v1.11.0 - Hotfix for Jenkinsfile (#137)
    * added Jenkins file
    
    * changes in Jenkinfiles
    
    * changes in Jenkinfiles
    
    * changes in Jenkinfiles
    
    * changes in Jenkinfiles
    
    * changes in Jenkinfiles after git 404
    
    * version update
    
    * changes in Jenkinfiles after git 404
    
    * chore: updated creds as per prod jenkins
    
    * chore: updated creds as per prod jenkins
    
    * chore: updated creds as per prod jenkins
    
    * chore: updated creds as per prod jenkins
    
    * chore: updated creds as per prod jenkins
    
    * chore: updated jenkinsfile as per node error
    
    * chore: updated jenkinsfile as per git error
    
    * chore: updated jenkinsfile as per git error
    
    * chore: updated jenkinsfile as per git token error
    
    * chore: updated jenkinsfile as per slack error
    
    * chore: updated jenkinsfile as per slack error
    
    * chore: updated jenkinsfile as per toLong error
    
    * chore: formatting done
    
    * chore: version increase
    
    * chore: git token error fixed
    
    * chore: git token error fixed
    
    * chore: git token error fixed 2
    
    * chore: git token error fixed 2
    
    * chore: added pr template
    
    * chore: added pr template
    
    * chore: versions corrected and increased
    
    * chore: added branch name check
    
    * fix: slack message and Release name in git
    
    * chore: increased versions

--- core/src/shared/getClient.ts
+++ core/src/shared/getClient.ts
@@ -3,7 +3,7 @@ import { LMFeedClient } from "@likeminds.community/feed-js";
 export function initiateFeedClient() {
   const lmFeedClient = LMFeedClient.Builder()
     .setPlatformCode("rt")
-    .setVersionCode(22)
+    .setVersionCode(23)
     .build();
   return lmFeedClient;
 }
